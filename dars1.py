5b5c66de9f108689fa22e06f99c47f5f59fb1f20
coderisayev <isayevsaidabdulla6@gmail.com>
2026-02-28 15:55:50 +0500

    funksiyalar

---
+++ dars1.py
@@ -0,0 +1,107 @@
+#1
+# a=int(input('Son kiriting:'))
+# b=a**2
+# print(f'siz kiritgansoning kvadrati {b}')
+# 2
+# a=int(input('Son kiriting:'))
+# b=a**3
+# print(f'siz kiritgansoning kubi {b}')
+# 3
+# n = int(input("N ni kiriting:"))
+# factarial = 1
+# for i in range(1,n+1):
+    # factarial *=i
+# print(factarial)
+# 4
+# a=int(input('son kirit'))
+# if a%2==0:
+#     print('bu son juft')
+# else:
+#     print('bu son toq')
+# 5
+# 6
+# a=int(input('a='))
+# b=int(input('b='))
+# if a>b:
+#     print(f'bu son eng katasi {a}')
+# elif b>a:
+#     print(f'bu son eng katasi {b}')
+# 7
+# a=int(input('son kirit:'))
+# b=int(input('sonkirit:'))
+# c=(a+b)/2
+# print(f'siz kiritgan ikki soning o\'rtacha qiymati {c}')
+# 8
+# a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,]
+# b=sum(a)
+# print(b)
+# 9
+# def eng_kattatop(royhat):
+#     eng_katta = max(royhat)
+#     return eng_katta
+# print(eng_kattatop([10,20,30,40,50]))
+# 10
+# def eng_kichik(royhat):
+#     eng_kich=min(royhat)
+#     return eng_kich
+# print(eng_kichik([10,20,30,40,1]))
+# 11
+# def sozlar_soni(royhat):
+#     soz = royhat.split()
+#     return len(soz)
+# print(sozlar_soni(['salom']))
+# 12
+# def harf_soni(royhat):
+#     return len(royhat)
+# print(harf_soni(['salom']))
+# 13
+# def uzun_matn(royhat):
+#     matn=royhat.split()
+#     eng_uzun = max(matn , key=len)
+#     return eng_uzun
+# print(uzun_matn('salomsdwfgsdfgsdf siz bugun yaxshi ishqildingiz'))
+# 14
+# def teskari_matn(royhat):
+#     matn=royhat[::-1]
+#     return matn
+# print(teskari_matn(['salom']))
+#15
+#  def ortacha_qiymat(son):
+#     ortacha=sum(son)
+#     bol=ortacha/len(son)
+#     return bol
+# print(ortacha_qiymat([1,2,3,4,5,6,7,8,9]))
+# 16
+# yigindi=0
+# n=int(input('son'))
+# for i in range(1,n+1):
+#     yigindi+=i
+
+# print(yigindi)
+# 17
+# yigindi=1
+# n=int(input('son'))
+# for i in range(1,n+1):
+#     yigindi*=i
+
+#     print(yigindi)
+# 18
+# a=int(input('son'))
+# for i in range(2,a+1,2):
+#     print(i)
+# 19
+# a=int(input('son'))
+# for i in range(1,a+1,2):
+#     print(i)
+# 20 hato
+def musbat_son(son):
+    musbat= son>=0
+    return musbat
+print(musbat_son([-1,1,23,-7,85,-78,9,-65,7,-8,-9,-10,2,3,1]))
+# 21
+# def manfiy_son(son):
+#     manfiy=son<0
+#     return manfiy
+# print(manfiy_son([1,-7,-8,-9,4,52,1,6,31,-555,-11,-86,0]))
+
+       
